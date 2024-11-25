5cf795711c6f70ce11381050037cdbb7b65e499f
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-25 18:13:20 +0530

    added void/never functions

--- 02-BASICS/functions.js
+++ 02-BASICS/functions.js
@@ -1,14 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
-    //return num + 2;
-    return "Hello";
+    return num + 2;
+    //return "Hello" => invalid
 }
 function getUpper(val) {
     return val.toUpperCase();
 }
-function signUpUser(name, email, isPaid) {
-}
+function signUpUser(name, email, isPaid) { }
 var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
@@ -16,3 +15,25 @@ var myVal = addTwo(5);
 getUpper("Gopal");
 signUpUser("Gopal", "gp@gmail.com", false);
 loginUser("G", "g21@g.com");
+// function getVal(myVal: number): number {
+//   if (myVal > 5) {
+//     return true;
+//   }
+//   return "200 OK";
+// }
+var getHello = function (s) {
+    return "";
+};
+var heroes = ["iron-man", "batman", "dr.strange"];
+//const heroes = [1,2,3]
+heroes.map(function (hero) {
+    return "Hero is ".concat(hero);
+});
+//! declared type -> void
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+//! never (used to handle error)
+function handleError(errMsg) {
+    throw new Error(errMsg);
+}
--- 02-BASICS/functions.ts
+++ 02-BASICS/functions.ts
@@ -1,21 +1,47 @@
-function addTwo(num: number) {
-  //return num + 2;
-  return "Hello"
+function addTwo(num: number): number {
+  return num + 2;
+  //return "Hello" => invalid
 }
 
 function getUpper(val: string) {
   return val.toUpperCase();
 }
 
-function signUpUser(name: string, email: string, isPaid: boolean) {
+function signUpUser(name: string, email: string, isPaid: boolean) {}
 
-}
-
-let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
 
 let myVal = addTwo(5);
 getUpper("Gopal");
-signUpUser("Gopal", "gp@gmail.com", false)
-loginUser("G", "g21@g.com")
+signUpUser("Gopal", "gp@gmail.com", false);
+loginUser("G", "g21@g.com");
+
+// function getVal(myVal: number): number {
+//   if (myVal > 5) {
+//     return true;
+//   }
+//   return "200 OK";
+// }
+
+let getHello = (s: string): string => {
+  return "";
+};
+
+const heroes = ["iron-man", "batman", "dr.strange"];
+//const heroes = [1,2,3]
+
+heroes.map((hero): string => {
+  return `Hero is ${hero}`;
+});
+
+//! declared type -> void
+function consoleError(errmsg: string) {
+  console.log(errmsg);
+}
+
+//! never (used to handle error)
+function handleError(errMsg: string): never {
+  throw new Error(errMsg);
+}
 
 export {};
