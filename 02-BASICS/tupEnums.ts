c4e38e2387c2c327f72f2ecd6ef09428227cf294
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-27 07:41:04 +0530

    added tuples

---
+++ 02-BASICS/tupEnums.ts
@@ -0,0 +1,17 @@
+//. <==TUPLES==>
+
+let tUser: [string, number, boolean];
+tUser = ["go", 5, false];
+
+let rgb: [number, number, number] = [233, 533, 232];
+
+type User = [number, string];
+
+let user1: User = [23, "gopal"];
+
+user1[1] = "sonali";
+user1.push(4);
+
+//? Spread operatior in tuples
+type Data = [string, boolean, ...number[]] //! spread can be only used once (...)
+let data1: Data = ["Gopal", true, 34, 5, 23,98]
