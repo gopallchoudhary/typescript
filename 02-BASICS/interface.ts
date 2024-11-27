2511f4d9a2a3a35d64611cd0b54d461cb34ef21d
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-27 18:47:20 +0530

    added interface

--- 02-BASICS/interface.ts
+++ 02-BASICS/interface.ts
@@ -4,19 +4,42 @@ interface User {
   userid: number;
   googleId?: number;
   //!startTrial: () => string or startTrial(): string
-  startTrial(): string,
-  getCoupon(couponName: string, value: number): number
+  startTrial(): string;
+  getCoupon(couponName: string, value: number): number;
 }
 
-const gopal: User = {
+//? Reopening of the interface
+interface User {
+  githubToken: string;
+}
+
+//? Inheritence
+interface Admin extends User {
+  role: "admin" | "ta" | "manager";
+}
+
+const gopal: Admin = {
   dbId: 22,
   email: "go@gmail.com",
   userid: 1473,
+  githubToken: "github",
+  role: "manager",
   startTrial: () => {
-    return "trial started"
+    return "trial started";
   },
-  getCoupon: (name: "gopal", off: 10) =>  {
-    return 10
+  getCoupon: (name: "gopal", off: 10) => {
+    return 10;
   },
-  
 };
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+function getPoint(pt: Point) {
+  console.log(`The x is ${pt.x}`);
+  console.log(`The y is ${pt.y}`);
+}
+
+getPoint({ x: 23, y: 9 });
