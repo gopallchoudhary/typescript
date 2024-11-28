6c53c28179044d0eac4ddcb2422055e5d4ab2851
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-28 10:59:54 +0530

    added interface

--- 02-BASICS/interface.ts
+++ 02-BASICS/interface.ts
@@ -1,12 +1,12 @@
-interface User {
-  readonly dbId: number;
-  email: string;
-  userid: number;
-  googleId?: number;
-  //!startTrial: () => string or startTrial(): string
-  startTrial(): string;
-  getCoupon(couponName: string, value: number): number;
-}
+// interface User {
+//   readonly dbId: number;
+//   email: string;
+//   userid: number;
+//   googleId?: number;
+//   //!startTrial: () => string or startTrial(): string
+//   startTrial(): string;
+//   getCoupon(couponName: string, value: number): number;
+// }
 
 //? Reopening of the interface
 interface User {
@@ -18,19 +18,19 @@ interface Admin extends User {
   role: "admin" | "ta" | "manager";
 }
 
-const gopal: Admin = {
-  dbId: 22,
-  email: "go@gmail.com",
-  userid: 1473,
-  githubToken: "github",
-  role: "manager",
-  startTrial: () => {
-    return "trial started";
-  },
-  getCoupon: (name: "gopal", off: 10) => {
-    return 10;
-  },
-};
+// const gopal: Admin = {
+//   dbId: 22,
+//   email: "go@gmail.com",
+//   userid: 1473,
+//   githubToken: "github",
+//   role: "manager",
+//   startTrial: () => {
+//     return "trial started";
+//   },
+//   getCoupon: (name: "gopal", off: 10) => {
+//     return 10;
+//   },
+// };
 
 interface Point {
   x: number;
