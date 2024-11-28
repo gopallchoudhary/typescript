5cd19b3cdd36e07568145a4f4e81180ad9e28ef6
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-28 11:00:28 +0530

    added class

--- 03-PURETS/class.ts
+++ 03-PURETS/class.ts
@@ -10,14 +10,25 @@
 // }
 
 class User {
+    readonly city: string = "Raipur"
+    private _courseCount: number = 1
     constructor(
         public name: string,
         private email: string,
         //readonly userId: string
     ) {}
+
+    get getAppleEmail(): string {
+        return `Apple ${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
 }
 
 const Gopal = new User("Gopal", "go01@gmail.com")
 Gopal.name
 // Gopal.email (cannot accessible outside the class User as it is privte)
+
 
