b0ad3acab045712a74d97eb7524c19bc0e73a858
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-29 07:15:38 +0530

    added class

--- 03-PURETS/class.ts
+++ 03-PURETS/class.ts
@@ -9,26 +9,38 @@
 //   }
 // }
 
-class User {
-    readonly city: string = "Raipur"
-    private _courseCount: number = 1
-    constructor(
-        public name: string,
-        private email: string,
-        //readonly userId: string
-    ) {}
+//? Class
+// class User {
+//   readonly city: string = "Raipur";
+//   private _courseCount: number = 1;
+//   constructor(
+//     public name: string,
+//     private email: string //readonly userId: string
+//   ) {}
 
-    get getAppleEmail(): string {
-        return `Apple ${this.email}`
-    }
+  //? Getter
+  // get getAppleEmail(): string {
+  //   return `Apple ${this.email}`;
+  // }
 
-    get courseCount(): number {
-        return this._courseCount
-    }
-}
+  // get courseCount(): number {
+  //   return this._courseCount;
+  // }
 
-const Gopal = new User("Gopal", "go01@gmail.com")
-Gopal.name
+  //? Setter
+  // set courseCount(courseNum) {
+  //   if (courseNum <= 1) {
+  //     throw new Error("course count must be greater than 1");
+  //   }
+  //   this._courseCount = courseNum;
+  // }
+
+  //? private methods
+//   private deleteToken() {
+//     console.log("Token deleted");
+//   }
+// }
+
+// const Gopal = new User("Gopal", "go01@gmail.com");
+// Gopal.name;
 // Gopal.email (cannot accessible outside the class User as it is privte)
-
-
