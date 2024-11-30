54faa097841f79eec64598e46e3615caba5dbaab
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-30 06:34:47 +0530

    added protected specifier

--- 03-PURETS/class.ts
+++ 03-PURETS/class.ts
@@ -10,37 +10,44 @@
 // }
 
 //? Class
-// class User {
-//   readonly city: string = "Raipur";
-//   private _courseCount: number = 1;
-//   constructor(
-//     public name: string,
-//     private email: string //readonly userId: string
-//   ) {}
+class User {
+  readonly city: string = "Raipur";
+  protected _courseCount: number = 1;
+  constructor(
+    public name: string,
+    private email: string //readonly userId: string
+  ) {}
 
   //? Getter
-  // get getAppleEmail(): string {
-  //   return `Apple ${this.email}`;
-  // }
+  get getAppleEmail(): string {
+    return `Apple ${this.email}`;
+  }
 
-  // get courseCount(): number {
-  //   return this._courseCount;
-  // }
+  get courseCount(): number {
+    return this._courseCount;
+  }
 
   //? Setter
-  // set courseCount(courseNum) {
-  //   if (courseNum <= 1) {
-  //     throw new Error("course count must be greater than 1");
-  //   }
-  //   this._courseCount = courseNum;
-  // }
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("course count must be greater than 1");
+    }
+    this._courseCount = courseNum;
+  }
 
   //? private methods
-//   private deleteToken() {
-//     console.log("Token deleted");
-//   }
-// }
+  private deleteToken() {
+    console.log("Token deleted");
+  }
+}
 
-// const Gopal = new User("Gopal", "go01@gmail.com");
-// Gopal.name;
+class SubUser extends User {
+  isPaid: boolean = true
+  changeCourseCount() {
+    this._courseCount = 5
+  }
+}
+
+const Gopal = new User("Gopal", "go01@gmail.com");
+Gopal.name; 
 // Gopal.email (cannot accessible outside the class User as it is privte)
