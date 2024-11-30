81ade20a3494514182a61b434cd9d5fa53f2f918
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-30 09:53:39 +0530

    added array in generics

--- 03-PURETS/generics.ts
+++ 03-PURETS/generics.ts
@@ -13,13 +13,31 @@ function identityThree<Type>(val: Type): Type {
   return val;
 }
 
-identityThree("coder")
+identityThree("coder");
 function identityFour<T>(val: T): T {
+  //console.log(val.length); T is not an array
   return val;
 }
 
-const num = identityFour<number>(89) //? here T is number
+const num = identityFour<number>(89); //? here T is number
+const str = identityFour<string>("Gopal here"); //? here T is string
 
+//. Array in Generics 
+function xyzIdentity<T>(arg: T[]): T[] {
+  return arg;
+}
 
-const str = identityFour<string>("Gopal here") //? here T is string
+function logIdentity<T>(arg: Array<T>): Array<T> {
+  return arg;
+}
 
+function getSearchProducts<T>(products: T[]): T {
+  const myIndex: number = 8;
+  return products[myIndex];
+}
+
+//? Generic in arrow functions
+const getMoreSearchProducts = (products: number[]): number => {
+    const myIndex = 5;
+    return products[myIndex]
+}
