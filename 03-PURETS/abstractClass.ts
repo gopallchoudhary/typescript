bf825685292787ed024da3e1fadaf65d432d3db2
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-11-30 07:20:40 +0530

    added abstract class

---
+++ 03-PURETS/abstractClass.ts
@@ -0,0 +1,35 @@
+abstract class TakePhoto {
+    constructor (
+        public cameraMode: string,
+        public filter: string
+    ) {}
+    
+    abstract getSepia(): void
+    abstract getAqua(): void
+    getReelTime(): number {
+        //complex calculation
+        return 8
+    }
+}
+
+class Instagram extends TakePhoto{
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number
+    ) {
+        super(cameraMode, filter)
+    }
+
+    getSepia(): void {
+        console.log("sepia");   
+    }
+
+    getAqua(): void {
+        console.log("aqua");
+    }
+
+}
+
+//! cannot create object from TakePhoto class
+//? can be created from the Instagram class
